ae077c29645264c7f18b94c41445964020a283ff
hillel <a4105962@gmail.com>
2021-03-05 12:55:51 +0200

    cont

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,57 @@
 import './App.css';
+import React, { useEffect } from "react";
 import Login from './login'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+      <div className="App">
+        <div className="cardmnue">
+          <div className="nav-linkdiv">
+            <Link className='nav-link' to="/">דף הבית</Link>
+          </div>
+          <div className="nav-linkdiv">
+            <Link className='nav-link' to="/move">חיפוש סרט</Link>
+          </div>
+        </div>
+        <div className="cardto">
+          <Switch>
+            <Route path="/move">
+              <Move />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router >
+  );
+}
+
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-text">React - האתר הראשון שלי ב</div>
+    </div>
+  )
+}
+
+
+
+function Move() {
+  return (
     <div className="App">
       <header className="App-header">
-      <Login/>
+        <Login />
       </header>
     </div>
   );
