4899d5ef08acb5f1e89f9f67e70947cecfed6f23
hillel <a4105962@gmail.com>
2021-03-09 12:52:13 +0200

    cont

--- server.js
+++ server.js
@@ -67,6 +67,9 @@ app.post('/gettyperadio', async (req, res) => {
     res.send({ deta })
 })
 
+app.get('/*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
